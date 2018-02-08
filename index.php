0ef257ba2c18a8b5d42e1da8b5164bcba640a79e
Eros Gonzalez Lopez <egonzalez-lopez@csumb.edu>
2018-02-08 00:50:59 +0000

    homework1

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset= "utf-8">
+        <title> Coding Languanges </title>
+        <link href = "css/styles.css" rel = "stylesheet" type = "text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond" rel="stylesheet">
+    </head>
+    
+    <body style = "background-color:white; color:black">
+        <header>
+            <h1> Languange </h1>
+        </header>
+        
+        <nav>
+            <hr width = "50%" />
+            <a href = "index.php"><strong>Home</strong></a>
+            <a href = "C++.html">C++</a>
+            <a href = "Python.html">Python</a>
+            <a href = "Java.html">Java</a>
+        </nav>
+        
+        <main>
+            <figure id="image">
+                <img src = "http://icons.iconarchive.com/icons/thehoth/seo/256/seo-web-code-icon.png" />
+            </figure>
+            
+            <div id="welcomeText">
+                Welcome!!! <br />
+                <p>This website is dedicated to inform users on the variety of programming languages used in the industry. </p>
+                <p><strong>Here is a list of 3 popular coding languages covered</strong></p>
+                    <ul>
+                       <li><span class="language">C++</span>: 1979
+                       </li>
+                       
+                       <br />
+                       
+                       <li><span class="language">Python</span>: 1989
+                       </li>
+                       
+                       <br />
+                       
+                       <li><span class="language">Java</span>: 1995
+                       </li>
+                   </ul>
+
+                <br />
+                <div id ="music">  
+                        <ul>
+                            <li>Mystery Fun Song</li>
+                                <audio controls autoplay>
+                                    <source src="music/Chacarron_Macarron[Mp3Converter.net].mp3" type="audio/mpeg">
+                                </audio>
+                        </ul>
+                </div>  
+            </div>
+        </main>
+    
+        <footer>
+            <hr>
+                CST-336. 2018&COPY; Gonzalez-Lopez<br />
+                 <br />
+            </hr>
+            
+            <figure id = "csumbLogo">
+                <img src="img/Cal-State-Monterey-Bay.jpg"></img>
+            </figure>
+        </footer>
+
+        
+    </body>
+
+</html>
